f7eed7f3ce96a585d13a650729085bddf175d031
GabrielKuiawa <gabrielsantos77239@gmail.com>
2025-10-08 04:20:25 -0300

    merge branch 'feature/database-schema' into feature/database-schema

--- backend/app/Controllers/HomeController.php
+++ backend/app/Controllers/HomeController.php
@@ -11,4 +11,24 @@ class HomeController extends Controller
         $title = 'Home Page';
         $this->render('home/index', compact('title'));
     }
+    public function getAllUsers(): void
+    {
+        // $users = User::all();
+
+        $result = [];
+
+        // foreach ($users as $user) {
+        $result[] = [
+            'id' => "ejr2k3jmik4j23",
+            'name' => "John Doe",
+            'email' => "teste@gmal",
+            'is_manager' => true,
+            'profile_picture_url' => "https://example.com/profile.jpg",
+        ];
+        // }
+
+        http_response_code(200);
+        header('Content-Type: application/json');
+        echo json_encode($result);
+    }
 }
