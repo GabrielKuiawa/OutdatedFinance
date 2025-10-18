a9ed4e31d674cbea69bee1b4f0b83251eccc1be8
GabrielKuiawa <gabrielsantos77239@gmail.com>
2025-10-18 07:37:49 -0300

    feat: add Gluestack UI provider with theme support and nativewind integration

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -2,12 +2,19 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function Layout() {
   return (
-    <Stack>
+    
+    <GluestackUIProvider mode="dark">
+      <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="home" options={{ title: 'Tela Inicial usuário' }} />
       <Stack.Screen name="homeAdmin" options={{ title: 'Tela Inicial Administrador' }} />
     </Stack>
+    </GluestackUIProvider>
+  
   );
 }
